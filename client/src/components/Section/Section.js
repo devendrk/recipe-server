0f1f556363646d51f2bac924d489b4be2501d128
Devendra Katwal <katwaldevendra1@gmail.com>
2021-06-18 14:17:58 +0300

    removed client

--- client/src/components/Section/Section.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Section.scss";
-
-const Section = ({ children }) => {
-  return <div className="section__container">{children}</div>;
-};
-
-export default Section;
